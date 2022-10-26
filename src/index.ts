9fc01716d0e9f52d254028ec9acfcac6f13be34c
cerensolpan <cerensolpan@gmail.com>
2022-10-27 00:21:39 +0300

    feat: Update index.html and get elements on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+form?.addEventListener("submit", e =>{
+  e.preventDefault()
+})
